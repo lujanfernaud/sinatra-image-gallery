cdb73cb8401607d3fd183113070133a41528cadd
lujanfernaud <lujanfernaud@gmail.com>
2016-09-08 07:41:12 +0100

    Add share button to lightbox, restyle lightbox

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-$(document).on("click", "#edit-button", function(){
+$(document).on("click", "#edit-button", function() {
   var imageId    = $(this).data("image-id");
   var imageFile  = $(this).data("image-file");
   var imageTitle = $(this).data("image-title");
@@ -8,10 +8,18 @@ $(document).on("click", "#edit-button", function(){
   $("#form-input-text", "#modal-edit").val(imageTitle);
 });
 
-$(document).on("click", "#delete-button", function(){
+$(document).on("click", "#delete-button", function() {
   var imageId    = $(this).data("image-id");
   var imageFile  = $(this).data("image-file");
 
   $("#form-delete", "#modal-delete").attr("action", "/images/" + imageId + "/delete");
   $("#modal-image", "#modal-delete").attr("src", imageFile);
+});
+
+$(document).on("click", "#thumbnail", function() {
+  var imageId    = $(this).data("image-id");
+
+  $("#share-button").click(function() {
+    $("#form-input-text", "#modal-share").val("https://localhost:9292/images/" + imageId);
+  });
 });
